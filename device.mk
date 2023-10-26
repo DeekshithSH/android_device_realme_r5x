091c82370018556d1f8a243e3c04d4f9f18753c2
AmritoRock <amritokarmokar5i@gmail.com>
2023-10-26 13:41:45 +0600

    r5x: Use prebuilt libhidlbase v32 for Oppo health service
    - Error
    
    CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.health@2.0-service.oppo": cannot locate symbol "_ZN7android8String16aSEOS0_" referenced by "/apex/com.android.vndk.v34/lib64/libhidlbase.so
    
    To fix it, copy the prebuilt libhidlbase from v32 prebuilts and patch the health service to use it
    
    Signed-off-by: Amritorock <amritokarmokar5i@gmail.com>

--- device.mk
+++ device.mk
@@ -473,6 +473,10 @@ PRODUCT_COPY_FILES += \
 # Vibrator
 $(call inherit-product, vendor/qcom/opensource/vibrator/vibrator-vendor-product.mk)
 
+# VNDK
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlbase-v32.so
+
 # V4a
 $(call inherit-product-if-exists, packages/apps/ViPER4AndroidFX/config.mk)
 
